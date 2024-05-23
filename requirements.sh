0d4e476f809e25833b1a8287e9f384c8f4255be3
hhhmgk <ngoduchuy71123@gmail.com>
2024-05-23 23:33:56 +0700

    change bash

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,8 @@
 TORCH=2.2.2
 CUDA=cu121
-pip install torch==2.2.2  --index-url https://download.pytorch.org/whl/cu121
-pip install ogb pykeops
-pip install torchdiffeq
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-geometric
-pip install -r requirements2.txt
+pip install torch==2.2.2  --index-url https://download.pytorch.org/whl/cu121 -qq
+pip install ogb pykeops -qq
+pip install torchdiffeq -qq
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html -qq
+pip install torch-geometric -qq
+pip install -r requirements2.txt -qq
